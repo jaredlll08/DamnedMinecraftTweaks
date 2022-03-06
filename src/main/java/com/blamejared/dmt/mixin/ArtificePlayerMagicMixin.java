e3cebb8f5754c2a6212b559480ed9c6a240ef540
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2022-03-06 22:41:19 +0100

    remap!

--- src/main/java/com/blamejared/dmt/mixin/ArtificePlayerMagicMixin.java
+++ src/main/java/com/blamejared/dmt/mixin/ArtificePlayerMagicMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerMagic.class)
 public class ArtificePlayerMagicMixin  {
 
-    @Inject(method = "tick", at = {@At(value = "HEAD")})
+    @Inject(method = "tick", at = {@At(value = "HEAD")}, remap = false)
     private void tick(PlayerEntity regen_pct_per_tick, CallbackInfo ci){
         PlayerMagic playerMagic = (PlayerMagic) ((Object) this);
         playerMagic.getCastingResource().addModifier("dmct_wisdom", (int) Load.Unit(regen_pct_per_tick).getUnit().getCalculatedStat(ExileDB.Stats().get(AllAttributes.WIS_ID)).getValue());
